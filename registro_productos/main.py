b76fd913134c99042b9c72915c2cdba21ddafea7
junnsarmiento-rgb <junnsarmient.o@gmail.com>
2026-05-05 09:48:38 -0500

    Update main.py

--- registro_productos/main.py
+++ registro_productos/main.py
@@ -1,1 +1,7 @@
-import tkinter as tk
+if __name__ == "__main__":
+    modelo = Producto()
+
+    vista = registroVista(controlador=None)
+    controlador = ProductoControlador(modelo, vista)
+    
+    controlador.ejecutar()
